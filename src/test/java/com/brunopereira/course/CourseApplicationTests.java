d363a1567cc0855274f9eac9f6016fee26d49618
Bruno Emanuel Pereira <bruno_emanuel87@hotmail.com>
2025-05-18 17:43:03 -0300

    First commit

---
+++ src/test/java/com/brunopereira/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunopereira.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
